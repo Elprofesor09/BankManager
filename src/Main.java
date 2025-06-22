e074ae0d31284190df46be280cf0c0a406a33459
youssouf <you@example.com>
2025-06-22 11:20:57 +0000

    Mise à jour

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        // Compte courant
+
         CheckingAccount checking = new CheckingAccount();
         checking.account = "CC001";
         checking.balance = 250000;
         checking.limit = 50000;
 
-        // Compte épargne
         SavingsAccount savings = new SavingsAccount();
         savings.account = "SA001";
         savings.balance = 150000;
         savings.interestRate = 0.05;
 
-        // Certificat de dépôt
+
         COD cod = new COD();
         cod.account = "CD001";
         cod.balance = 500000;
         cod.duration = 12;
 
-        // Affichage
         System.out.println("Compte courant : " + checking.account + ", Solde : " + checking.balance + ", Limite : " + checking.limit);
         System.out.println("Compte épargne : " + savings.account + ", Solde : " + savings.balance + ", Taux : " + savings.interestRate);
         System.out.println("COD : " + cod.account + ", Solde : " + cod.balance + ", Durée : " + cod.duration + " mois");
